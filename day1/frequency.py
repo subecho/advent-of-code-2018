ebb9d4220029312f974b254a9e2dae2ab733ccfb
Dustin Schoenbrun <dustin@subecho.org>
2018-12-10 11:21:22 -0500

    Completed Day 1 of the Advent of Code

--- day1/frequency.py
+++ day1/frequency.py
@@ -3,17 +3,35 @@ from optparse import OptionParser
 
 logger = logging.getLogger(__name__)
 
-def process_input_file(filename: str):
-    frequency = 0
+def load_operations_from_file(filename: str) -> list:
+    operations = []
     with open(filename, 'r') as fp:
         for line in fp:
-            operation = line[0]
-            value = int(line[1::])
+            operations.append(line.rstrip())
+    return operations
+
+def find_repeated_frequency(operations: list) -> int:
+    """Find the first repeated frequency in a list of operations"""
+    frequency = 0
+    seen_frequencies = []
+    number_of_iterations = 0
+    while True:
+        number_of_iterations += 1
+        logger.info("Starting iteration %s through operations" % number_of_iterations)
+        for op in operations:
+            operation = op[0]
+            value = int(op[1::])
             if operation == '+':
                 frequency = increase_frequency(value, frequency)
             else:
                 frequency = decrease_frequency(value, frequency)
-    return frequency
+            if frequency in seen_frequencies:
+                logger.info("Found Repeated Frequency %s" % frequency)
+                return frequency
+            else:
+                logger.info("Frequency %s not seen before, adding to list of "
+                            "seen frequencies" % frequency)
+                seen_frequencies.append(frequency)
 
 def increase_frequency(value: int, current_frequency: int) -> int:
     logger.info('Increasing Frequency by %s (%s)' % (value, current_frequency + value))
@@ -30,4 +48,5 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     loglevel = getattr(logging, options.loglevel)
     logging.basicConfig(level=loglevel)
-    print(process_input_file('input'))
+    operations = load_operations_from_file('input')
+    print("The First Repeated Frequency is: %s" % find_repeated_frequency(operations))
